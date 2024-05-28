e2a759381259b3cf74859563c463a753bf9778fc
Joshua Zhou <j.zhou11@share.epsb.ca>
2024-05-28 09:10:26 -0600

    Change box to target

--- game.py
+++ game.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
     crosshair.setScale(0.01)
     crosshair.setPosition(window.getVirtualWidth()//2-crosshair.getWidth()//2, window.getVirtualHeight()//2-crosshair.getHeight()//2)
 
-    boxes = []
+    targets = []
     for i in range(3):
-        boxes.append(Box(10,10, randint(0,window.getVirtualWidth()), randint(0,window.getVirtualHeight()), speed=0.32))
+        targets.append(Box(10,10, randint(0,window.getVirtualWidth()), randint(0,window.getVirtualHeight()), speed=0.32))
 
     pygame.mouse.set_pos(window.getVirtualWidth() // 2, window.getVirtualHeight() // 2)
     pygame.mouse.set_visible(False)
@@ -35,20 +35,20 @@ if __name__ == "__main__":
             elif event.type == pygame.MOUSEMOTION:
                 # Get mouse movement
                 mouse_x, mouse_y = event.rel
-                for box in boxes:
-                    box.x += mouse_x*box.speed
-                    box.y += mouse_y*box.speed
+                for target in targets:
+                    target.x += mouse_x*target.speed
+                    target.y += mouse_y*target.speed
                 # Center the mouse cursor again
                 pygame.mouse.set_pos(window.getVirtualWidth() // 2, window.getVirtualHeight() // 2)
 
         keys_pressed = pygame.key.get_pressed()
 
-        for box in boxes:
-            box.WASDMove(keys_pressed)
+        for target in targets:
+            target.WASDMove(keys_pressed)
 
         window.clearScreen()
         window.getScreen().blit(bg_img.getSurface(), bg_img.getPosition())
-        for box in boxes:
-            window.getScreen().blit(box.getSurface(), box.getPosition())
+        for target in targets:
+            window.getScreen().blit(target.getSurface(), target.getPosition())
         window.getScreen().blit(crosshair.getSurface(), crosshair.getPosition())
         window.updateFrame()
