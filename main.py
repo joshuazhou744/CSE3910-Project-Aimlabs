e5722661517e15a32dfe1411fe62772052f243c7
Kevin Li <k.li21@share.epsb.ca>
2024-05-31 11:33:34 -0600

    finished webshot

--- main.py
+++ main.py
@@ -76,32 +76,54 @@ def gridshot(window):
         pygame.display.update()
 
 def webshot():
+    score = Score()
+    counter = 0
+    text = Text(text=f"Shots: {score.shots} Hits: {score.hits} Accuracy: {score.accuracy}", font=get_font(24),
+                color=Color.black)
+
+    crosshair = ImageSprite("assets/crosshair.png")
+    crosshair.setScale(0.01)
+    crosshair.setPosition(window.getVirtualWidth() // 2 - crosshair.getWidth() // 2,
+                          window.getVirtualHeight() // 2 - crosshair.getHeight() // 2)
+
+    pygame.mouse.set_visible(False)
+
     target = Target("assets/target.png", 0, 0)
     target.setScale(0.1)
     target.setPosition(400 - target.getWidth() // 2, 300 - target.getHeight() // 2)
     while True:
-        web_mouse_pos = pygame.mouse.get_pos()
-
-        window.getScreen().blit(bg_img.getSurface(), bg_img.getPosition())
-        window.getScreen().blit(target.getSurface(), target.getPosition())
-
-        web_back = Button(pos=(window.getVirtualWidth()-100, window.getVirtualHeight()-30),
-                           text_input="Main Menu", font=get_font(30),base_color=Color.white, hover_color=Color.green)
-        web_back.changeColor(web_mouse_pos)
-        web_back.update(window.getScreen())
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if web_back.checkForInput(web_mouse_pos):
-                    main_menu()
-            if target.isCollision(25, 25, (window.getVirtualWidth() // 2, window.getVirtualHeight() // 2)):
-                target.setPosition(randint(0, window.getVirtualWidth() - target.getWidth()),
-                   randint(0, window.getVirtualHeight() - target.getHeight()))
+            elif event.type == pygame.MOUSEMOTION:
+                # Get mouse movement
+                mouse_x, mouse_y = event.rel
+                target.x += mouse_x*target.speed
+                target.y += mouse_y*target.speed
 
+                target.updatePosition()
+                # Center the mouse cursor again
+                pygame.mouse.set_pos(window.getVirtualWidth() // 2, window.getVirtualHeight() // 2)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                score.shots += 1
+                score.updateAccuracy()
+                text.setText(f"Shots: {score.shots} Hits: {score.hits} Accuracy: {score.accuracy}")
+                if target.isCollision(25, 25, (window.getVirtualWidth() // 2, window.getVirtualHeight() // 2)):
+
+                    target.setPosition(randint(0, window.getVirtualWidth() - target.getWidth()),
+                       randint(0, window.getVirtualHeight() - target.getHeight()))
+                    score.hits += 1
+                    score.updateAccuracy()
+                    text.setText(f"Shots: {score.shots} Hits: {score.hits} Accuracy: {score.accuracy}")
+
+        window.clearScreen()
+        window.getScreen().blit(bg_img.getSurface(), bg_img.getPosition())
+        window.getScreen().blit(text.getSurface(), text.getPosition())
+        window.getScreen().blit(target.getSurface(), target.getPosition())
+        window.getScreen().blit(crosshair.getSurface(), crosshair.getPosition())
         pygame.display.update()
+
 
 
 def flashshot():
